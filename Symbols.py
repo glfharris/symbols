85716b6d3c3851e9cfdf3b810be894dcc90c7bd8
George L. F. Harris <glfharris@gmail.com>
2018-08-28 13:18:11 +0100

    Math Fix

--- Symbols.py
+++ Symbols.py
@@ -26,13 +26,13 @@ def onAnkiSymbols(self):
     favourites = QMenu("Favourites", self.mw)
     greek = QMenu("Greek letters", self.mw)
     arrows = QMenu("Arrows", self.mw)
-    math = QMenu("Maths", self.mw)
+    maths = QMenu("Maths", self.mw)
 
     # Adding submenus to main menu
     main.addMenu(favourites)
     main.addMenu(greek)
     main.addMenu(arrows)
-    main.addMenu(math)
+    main.addMenu(maths)
 
     # Adding symbols to sub menus
     # Greek Letters 913 - 974
@@ -52,7 +52,7 @@ def onAnkiSymbols(self):
 
     # Maths
     for math in [8800, 8776, 8804, 8805, 8733, 8734]:
-        a = arrows.addAction(unichr(math))
+        a = maths.addAction(unichr(math))
         a.connect(a, SIGNAL("triggered()"), onAddAnkiSymbol_factory(self, math))
     
     main.exec_(QCursor.pos())
