e3c8c925ae423f281dffc73d9b1b0587b6549b03
George L. F. Harris <glfharris@gmail.com>
2024-02-10 16:49:34 +0000

    Modernisation

--- symbols/symbols.py
+++ symbols/symbols.py
@@ -1,22 +1,26 @@
+import json
+
+from anki import version as anki_version
+from anki.hooks import addHook, wrap
+
 from aqt import mw
 from aqt.qt import *
 from aqt.editor import Editor
-from anki.hooks import addHook, wrap
 
+ANKI_VERSION_TUPLE = tuple(int(i) for i in anki_version.split("."))
 
 config = mw.addonManager.getConfig(__name__)
-
 
 def onSymbolButton(self):
     main = QMenu(mw)
 
-    last = main.addAction("Last Used: %s" % config["last_used"])
+    last = main.addAction(f"Last Used: {config['last_used']}")
     last.triggered.connect(symbolFactory(self, config["last_used"]))
 
     faves = QMenu("Favourites", mw)
     main.addMenu(faves)
 
-    faves_list = list(config['favourites'])
+    faves_list = list(config["favourites"])
     char_sets = config["char_sets"]
 
     for char in faves_list:
@@ -40,16 +44,19 @@ def symbolFactory(editor, symbol):
     return lambda: add_char(editor, symbol)
 
 
-def add_char(editor, char):
+def add_char(editor: Editor, char):
     config["last_used"] = char
     mw.addonManager.writeConfig(__name__, config)
-    editor.web.eval("wrap('%s', '');" % char)
+    editor.web.eval(f"pasteHTML({json.dumps(char)}, true, false);")
 
 
-def setupButtons(buttons, editor):
+def setupButtons(buttons, editor: Editor):
     short_cut = config.get("short_cut", "ctrl+s")
-    b = editor.addButton(
-        None, 'Sym', onSymbolButton, tip="Inserts Symbols at Cursor (%s)" % short_cut, keys=short_cut)
+    b = editor.addButton( None, 'Sym', onSymbolButton,
+                         tip=f"Inserts symbol at cursor ({short_cut})",
+                         keys=short_cut,
+                         rightside=ANKI_VERSION_TUPLE < (2,1,50),
+                    )
     buttons.append(b)
 
     return buttons
